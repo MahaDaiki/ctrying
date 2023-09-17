2a13c05526a4be5001c2f7aea4d7c06495e4d09a
MahaDaiki <mahadaiki2@gmail.com>
2023-09-17 22:12:50 +0200

    meme

---
+++ crazy.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main () {
+	while (1){
+		printf( " Crazy ?\n");
+		printf("I was crazy once, they locked me in a room, a rubber room, a rubber room with rats, and rats made me crazy");
+	}
+return 0;
+}
