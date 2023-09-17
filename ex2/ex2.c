fd19c48920035020d551101c0e3bf9ec15cad5bc
MahaDaiki <mahadaiki2@gmail.com>
2023-09-17 20:03:20 +0200

    ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,25 +1,30 @@
 #include<stdio.h>
 
 int main (){
-	int i, num[9];
-	while (1) {
+	int i; 
+	char num[10],c;
+	do {
 		printf("\nEnter your number:\n ");
 		for(i=0;i<10;i++){
-			scanf("%d",&num[i]);
+			scanf("%c",&num[i]);
 		}
-		if ( num[1]==6){
+		if ( num[1]=='6'){
 			printf("Mobile: +212");
-			for(i=0;i<10;i++){
-			printf("%d",num[i]);
+			for(i=1;i<10;i++){
+			printf("%c",num[i]);
 		}
 		}
-		else  if ( num[1]==5) {
+		else  if ( num[1]=='5') {
 			printf("Fix : +212");
-			for(i=0;i<10;i++){
-			printf("%d",num[i]);
+			for(i=1;i<10;i++){
+			printf("%c",num[i]);
 			}
 		}
-		}
+	printf("\nDo you want to enter another number? (y/n): ");
+        scanf(" %c", &c);
+
+    } while (c == 'y' || c == 'Y');
+
 
 return 0 ;
 }
