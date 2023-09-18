e2c195bb36ff7fe7f2cb9d95c95a704c45a2d0f3
MahaDaiki <mahadaiki2@gmail.com>
2023-09-18 11:16:56 +0100

    ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,30 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main (){
-	int i; 
-	char num[10],c;
-	do {
-		printf("\nEnter your number:\n ");
-		for(i=0;i<10;i++){
-			scanf("%c",&num[i]);
-		}
-		if ( num[1]=='6'){
-			printf("Mobile: +212");
-			for(i=1;i<10;i++){
-			printf("%c",num[i]);
-		}
-		}
-		else  if ( num[1]=='5') {
-			printf("Fix : +212");
-			for(i=1;i<10;i++){
-			printf("%c",num[i]);
-			}
-		}
-	printf("\nDo you want to enter another number? (y/n): ");
-        scanf(" %c", &c);
+void handlingNumberMobile() {
+    char num[11]; // Increase the size of the array to 11 to store 10 characters and a null terminator.
+    int i;
 
-    } while (c == 'y' || c == 'Y');
+    printf("\nEnter your number:\n ");
+    
+    // Use ' %c' in scanf to skip newline characters and spaces.
+    for (i = 0; i < 10; i++) {
+        scanf(" %c", &num[i]);
+    }
+    num[10] = '\0'; // Null-terminate the string.
 
+    if (num[1] == '6') {
+        printf("Mobile: +212%s\n", num + 1); // Print the number starting from the second character.
+    } else if (num[1] == '5') {
+        printf("Fix : +212%s\n", num + 1); // Print the number starting from the second character.
+    }
+}
+int main() {
+    int choice;
 
-return 0 ;
+    while (1) {
+        printf("1: Enter number\n");
+        printf("2: Exit\n");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                handlingNumberMobile();
+                break;
+            case 2:
+                return 0;
+            default:
+                printf("Invalid choice. Please select 1 or 2.\n");
+        }
+    }
 }
